a32e1222d1f0c4c6ddffcdf4507d4785b276ec5d
Misael <misael.alisson14@gmail.com>
2024-07-24 16:35:11 -0300

    add open close functionality to modal
    modal component was transferred inside the header component, for simplicity, since the button to open the modal is there

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
+"use client"
 import { AddTransactionModal } from "@/components/AddTransactionModal";
 import { Header } from "@/components/Header/Index";
 import { List } from "@/components/List";
 import { ValuesCard } from "@/components/ValuesCard";
 import { ValuesCardGroup } from "@/components/ValuesCardGroup";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
+  const [openModal, setOpenModal] = useState(false)
+
   return (
     <>
-      <AddTransactionModal/>
+      
       <Header />
       <div className="mx-auto max-w-[1120px]">
         <ValuesCardGroup />
--- src/components/Header/Index.tsx
+++ src/components/Header/Index.tsx
@@ -1,10 +1,26 @@
 import Image from "next/image";
+import { AddTransactionModal } from "../AddTransactionModal";
+import { useState } from "react";
 
 export function Header() {
-    return <header className="bg-header h-[212px] w-full">
+  const [openModal, setOpenModal] = useState(false);
+    return (
+    <>
+      <AddTransactionModal isOpen={openModal} setIsOpen={setOpenModal}/>
+      <header className="bg-header h-[212px] w-full">
         <div className="mx-auto flex justify-between max-w-[1120px] pt-8">
-            <Image className="max-h-10" width={172} height={40} src="/images/logo.png" alt="logo" />
-        <button className="bg-button text-white size-4 w-[195px] h-[48px] = rounded-md text-center flex items-center justify-center hover:opacity-90">Nova Transação</button>
-    </div>  
-    </header>
+          <Image
+            className="max-h-10"
+            width={172}
+            height={40}
+            src="/images/logo.png"
+            alt="logo"
+          />
+          <button onClick={() => setOpenModal(true)} className="bg-button text-white size-4 w-[195px] h-[48px] = rounded-md text-center flex items-center justify-center hover:opacity-90">
+            Nova Transação
+          </button>
+        </div>
+      </header>
+    </>
+  );
 }
